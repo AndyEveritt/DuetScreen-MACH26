292f59044d3853515bcd6533ed632ebfffdca59e
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2024-12-13 12:24:25 +0000

    allow View to have specific parent to allow subviews

--- src/UI/Core/view.h
+++ src/UI/Core/view.h
@@ -17,23 +17,38 @@ namespace UI
 	class View
 	{
 	  public:
-		View(lv_coord_t xPct = 0, lv_coord_t yPct = 0, lv_coord_t widthPct = 100, lv_coord_t heightPct = 100)
-			: m_cont(lv_obj_create(lv_scr_act()))
+		View(lv_obj_t* parent, lv_coord_t xPct, lv_coord_t yPct, lv_coord_t widthPct, lv_coord_t heightPct)
+			: m_cont(lv_obj_create(parent))
 			, m_presenter(0)
 		{
 			lv_obj_set_pos(m_cont, lv_pct(xPct), lv_pct(yPct));
 			lv_obj_set_size(m_cont, lv_pct(widthPct), lv_pct(heightPct));
 		}
+		View(lv_coord_t xPct, lv_coord_t yPct, lv_coord_t widthPct, lv_coord_t heightPct)
+			: View(lv_scr_act(), xPct, yPct, widthPct, heightPct)
+		{
+		}
+		View(lv_obj_t* parent)
+			: View(parent, 0, 0, 100, 100)
+		{
+		}
 		virtual ~View() {}
 
 		/**
-		 * Binds an instance of a specific Presenter type (subclass) to the View instance. This function
-		 * is called automatically when a new presenter/view pair is activated.
-		 *
-		 * @param [in] newPresenter The specific Presenter to be associated with the View.
+		 * @brief Binds an instance of a specific Presenter type (subclass) to the View instance. This function is
+		 * called automatically when a new presenter/view pair is activated.
+		 * @param newPresenter The specific Presenter to be associated with the View.
 		 */
 		void bind(T& newPresenter) { m_presenter = &newPresenter; }
+
+		/**
+		 * @brief Get a pointer to the MVP model
+		 */
 		Model* getModel() { return m_presenter->getModel(); }
+
+		/**
+		 * @return Get the base container for the view
+		 */
 		lv_obj_t* getBaseContainer() { return m_cont; }
 
 		virtual void init() {}
