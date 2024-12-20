f9ec421b3601c0f85a4e68b223b959fc2e107fee
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2024-12-20 09:27:27 +0000

    Update lvgl build

--- libraries/lvgl.cmake
+++ libraries/lvgl.cmake
@@ -5,6 +5,8 @@ option(LV_USE_LIBJPEG_TURBO "Use libjpeg turbo to decode JPEG" OFF)
 option(LV_USE_FFMPEG "Use libffmpeg to display video using lv_ffmpeg" OFF)
 option(LV_USE_FREETYPE "Use freetype library" OFF)
 
+option(LV_CONF_BUILD_DISABLE_DEMOS "Disable building of demos" ON)
+option(LV_CONF_BUILD_DISABLE_EXAMPLES "Disable building of examples" ON)
 
 # Add compile definitions based on the selected options
 add_compile_definitions($<$<BOOL:${LV_USE_DRAW_SDL}>:LV_USE_DRAW_SDL=1>)
@@ -17,3 +19,4 @@ add_compile_definitions($<$<BOOL:${USE_FREERTOS}>:LV_USE_OS=2>)
 # Add LVGL subdirectory
 add_subdirectory(${LIBRARIES_DIR}/lvgl)
 target_include_directories(lvgl PUBLIC ${PROJECT_SOURCE_DIR} ${SDL2_INCLUDE_DIRS} ${LIBRARIES_DIR})
+# target_compile_definitions(lvgl PUBLIC ESP_PLATFORM=0)
