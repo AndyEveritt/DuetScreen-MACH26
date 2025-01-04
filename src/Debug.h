b1d25a3875d5cec737f5743d61548a7cf7f8f63d
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2025-01-04 17:42:55 +0000

    log color enum

--- src/Debug.h
+++ src/Debug.h
@@ -31,6 +31,18 @@ enum class DebugLevel
 	COUNT
 };
 
+enum class LogColors
+{
+	Black = 30,
+	Red,
+	Green,
+	Yellow,
+	Blue,
+	Magenta,
+	Cyan,
+	White,
+};
+
 static_assert(ARRAY_SIZE(DebugLevelStrings) == (int)DebugLevel::COUNT);
 
 void SetDebugLevel(DebugLevel level);
@@ -46,26 +58,30 @@ void fatal_inner(const char* fmt, ...);
 #define LOG_FUNCTION(name, color, level, fmt, args...)                                                                 \
 	do                                                                                                                 \
 	{                                                                                                                  \
-		name##_inner("\033[1;" #color "m%s\033[0m \033[3;4;" #color "m%s:%d %s:\033[0m\033[" #color "m " fmt           \
-					 "\033[0m\n",                                                                                      \
+		const int colorCode = static_cast<int>(color);                                                                 \
+		name##_inner("\033[1;%dm%s\033[0m \033[3;4;%dm%s:%d\033[0m \033[3;%dm%s():\033[0m\033[%dm " fmt "\033[0m\n",   \
+					 colorCode,                                                                                        \
 					 DebugLevelStrings[(int)level],                                                                    \
+					 colorCode,                                                                                        \
 					 __FILE_RELPATH__,                                                                                 \
 					 __LINE__,                                                                                         \
+					 colorCode,                                                                                        \
 					 __FUNCTION__,                                                                                     \
+					 colorCode,                                                                                        \
 					 ##args);                                                                                          \
 	} while (0)
 
 #ifdef DEBUG
-#define verbose(fmt, args...) LOG_FUNCTION(verbose, 37, DebugLevel::Verbose, fmt, ##args)
-#define dbg(fmt, args...) LOG_FUNCTION(dbg, 34, DebugLevel::Debug, fmt, ##args)
+#define verbose(fmt, args...) LOG_FUNCTION(verbose, LogColors::White, DebugLevel::Verbose, fmt, ##args)
+#define dbg(fmt, args...) LOG_FUNCTION(dbg, LogColors::Blue, DebugLevel::Debug, fmt, ##args)
 #else
 #define verbose(fmt, args...)
 #define dbg(fmt, args...)
 #endif
 
-#define info(fmt, args...) LOG_FUNCTION(info, 32, DebugLevel::Info, fmt, ##args)
-#define warn(fmt, args...) LOG_FUNCTION(warn, 33, DebugLevel::Warn, fmt, ##args)
-#define error(fmt, args...) LOG_FUNCTION(error, 31, DebugLevel::Error, fmt, ##args)
-#define fatal(fmt, args...) LOG_FUNCTION(fatal, 35, DebugLevel::Fatal, fmt, ##args)
+#define info(fmt, args...) LOG_FUNCTION(info, LogColors::Green, DebugLevel::Info, fmt, ##args)
+#define warn(fmt, args...) LOG_FUNCTION(warn, LogColors::Yellow, DebugLevel::Warn, fmt, ##args)
+#define error(fmt, args...) LOG_FUNCTION(error, LogColors::Red, DebugLevel::Error, fmt, ##args)
+#define fatal(fmt, args...) LOG_FUNCTION(fatal, LogColors::Magenta, DebugLevel::Fatal, fmt, ##args)
 
 #endif /* JNI_DEBUG_HPP_ */
