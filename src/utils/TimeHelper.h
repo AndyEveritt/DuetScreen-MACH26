6074fa73dff2c5982cb0753a801cd666957089a5
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2025-01-27 10:13:14 +0000

    remove simulation check from timehelper

--- src/utils/TimeHelper.h
+++ src/utils/TimeHelper.h
@@ -10,14 +10,9 @@ class TimeHelper
 	 */
 	static long long getCurrentTime()
 	{
-#if SIMULATION
 		return std::chrono::duration_cast<std::chrono::milliseconds>(
 				   std::chrono::system_clock::now().time_since_epoch())
 			.count();
-#else
-#warning "TimeHelper::getCurrentTime() not implemented for this platform"
-		return 0;
-#endif
 	}
 
 	static bool setDateTime(const char* dateTime) { return true; }
