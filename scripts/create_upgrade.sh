26fd8c54f08c6e5f898b85016f770fd36893e925
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2025-04-15 16:20:34 +0100

    fix(upgrade): remove old assets when updating

--- scripts/create_upgrade.sh
+++ scripts/create_upgrade.sh
@@ -15,6 +15,9 @@ cp -r assets out/update/etc
 mkdir -p out/update/usr/lib
 cp ../buildroot-duetscreen/output/target/usr/lib/liblvgl* out/update/usr/lib/
 
+echo "rm -rf /etc/assets" > out/update/pre-update
+chmod +x out/update/pre-update
+
 echo "/etc/init.d/S20DuetScreen restart" > out/update/post-update
 chmod +x out/update/post-update
 
