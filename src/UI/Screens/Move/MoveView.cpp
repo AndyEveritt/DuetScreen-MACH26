363c8450bde0b3639802394e3a2baae6b31f95a0
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2025-07-22 13:43:37 +0100

    feat(UI) remove double border around generic axes list

--- src/UI/Screens/Move/MoveView.cpp
+++ src/UI/Screens/Move/MoveView.cpp
@@ -103,6 +103,8 @@ namespace UI
 		// m_genericAxisControls.setFlexGrow(1);
 		m_genericAxisControls.setMaxWidth(LV_PCT(20));
 		m_genericAxisControls.setListFlow(LV_FLEX_FLOW_ROW);
+		m_genericAxisControls.addStyle(Themes::getLvglStyles().no_border);
+		m_genericAxisControls.addStyle(Themes::getLvglStyles().pad_zero);
 
 		m_axisList.setFlexGrow(1);
 		m_axisList.setHeight(LV_PCT(100));
