8c92b2aebb54fde7f9e662e1917a04fc5c9526c2
Andy Everitt <38423143+AndyEveritt@users.noreply.github.com>
2025-08-08 17:33:51 +0100

    Update install_prerequisites.sh

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y \
     ninja-build \
     libusb-1.0-0-dev \
     pkg-config \
-    clang-format \
+    clang-format-19 \
     libavcodec-dev \
     libavformat-dev \
     libswscale-dev \
