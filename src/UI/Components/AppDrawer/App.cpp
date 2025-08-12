2e9aa5beaeaac098c96fc3a149acdef46f82b4f4
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2025-08-12 10:08:29 +0100

    chore(UI): remove excess padding around apps

--- src/UI/Components/AppDrawer/App.cpp
+++ src/UI/Components/AppDrawer/App.cpp
@@ -8,6 +8,7 @@
 #include "App.h"
 #include "Debug.h"
 #include "UI/Core/Navigation.h"
+#include "UI/Styles/Styles.h"
 
 namespace UI
 {
@@ -15,6 +16,8 @@ namespace UI
 		: ListItem(index, parent)
 		, m_button("app_button", getRoot())
 	{
+		addStyle(Themes::getLvglStyles().pad_zero);
+
 		setSize(LV_SIZE_CONTENT, LV_SIZE_CONTENT);
 		m_button.setSize(LV_PCT(100), LV_PCT(100));
 		m_button.setMinWidth(LV_SIZE_CONTENT);
