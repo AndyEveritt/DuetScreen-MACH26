4dffe2dfc8f8692f0a9c279d92d1486c5882b0d0
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2025-10-07 16:11:34 +0100

    chore(lib): fix version of hv

--- libraries/hv.cmake
+++ libraries/hv.cmake
@@ -4,7 +4,7 @@ set(BUILD_STATIC ON CACHE BOOL "Build static library")
 FetchContent_Declare(
   hv
   GIT_REPOSITORY https://github.com/ithewei/libhv.git
-  GIT_TAG master
+  GIT_TAG e2ba81baa2fd782a13ce4b2be7c181d8966442d4
 )
 FetchContent_MakeAvailable(hv)
 # target_include_directories(DuetScreen.lib PUBLIC ${hv_SOURCE_DIR})
