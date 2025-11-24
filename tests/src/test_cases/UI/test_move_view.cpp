991b89928c3d254e2484df47557d5b8cc3690acf
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2025-11-24 17:48:21 +0000

    fix(tests): remove outdated test

--- tests/src/test_cases/UI/test_move_view.cpp
+++ tests/src/test_cases/UI/test_move_view.cpp
@@ -33,15 +33,3 @@ TEST_F(TestMoveView, OpenNumberpad)
 	label->sendEvent(LV_EVENT_CLICKED, nullptr);
 	EXPECT_EQUAL_SCREENSHOT("move_view/numberpad.png");
 }
-
-TEST_F(TestMoveView, ExtrudeControl)
-{
-	LvObj* btn = move_view.getChildByName("axis_control.extruder_control.controls.distance_input.list.1");
-	ASSERT_NE(btn, nullptr);
-
-	btn->sendEvent(LV_EVENT_CLICKED, nullptr);
-	EXPECT_EQUAL_SCREENSHOT("move_view/extrude_feedrate_select.png");
-
-	btn->sendEvent(LV_EVENT_LONG_PRESSED, nullptr);
-	EXPECT_EQUAL_SCREENSHOT("move_view/extrude_feedrate_select_long.png");
-}
