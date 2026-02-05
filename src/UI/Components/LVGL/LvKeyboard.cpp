31cba72c920b5813bd9e0a07cc22ac97c1f2822e
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2026-02-05 10:35:56 +0000

    chore(UI): update keyboard close icon

--- src/UI/Components/LVGL/LvKeyboard.cpp
+++ src/UI/Components/LVGL/LvKeyboard.cpp
@@ -18,7 +18,7 @@ namespace UI
 		"1#", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", LV_SYMBOL_BACKSPACE, "\n",
         "ABC", "a", "s", "d", "f", "g", "h", "j", "k", "l", LV_SYMBOL_NEW_LINE, "\n",
         "_", "-", "z", "x", "c", "v", "b", "n", "m", ".", ",", ":", "\n",
-        LV_SYMBOL_CLOSE, LV_SYMBOL_LEFT, " ", LV_SYMBOL_RIGHT, LV_SYMBOL_OK, ""
+        LV_SYMBOL_KEYBOARD, LV_SYMBOL_LEFT, " ", LV_SYMBOL_RIGHT, LV_SYMBOL_OK, ""
     };
 	
 	static const lv_buttonmatrix_ctrl_t default_kb_ctrl_lc_map[] = {
@@ -34,7 +34,7 @@ namespace UI
 		"1#", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", LV_SYMBOL_BACKSPACE, "\n",
         "abc", "A", "S", "D", "F", "G", "H", "J", "K", "L", LV_SYMBOL_NEW_LINE, "\n",
         "_", "-", "Z", "X", "C", "V", "B", "N", "M", ".", ",", ":", "\n",
-        LV_SYMBOL_CLOSE, LV_SYMBOL_LEFT, " ", LV_SYMBOL_RIGHT, LV_SYMBOL_OK, ""
+        LV_SYMBOL_KEYBOARD, LV_SYMBOL_LEFT, " ", LV_SYMBOL_RIGHT, LV_SYMBOL_OK, ""
     };
 	// clang-format on
 
