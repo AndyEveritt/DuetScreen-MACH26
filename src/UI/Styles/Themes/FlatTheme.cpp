c1f4824b3d2f9632c806be91c95ed921c55c2606
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2026-03-05 13:45:41 +0000

    chore(UI): update flat theme default colours

--- src/UI/Styles/Themes/FlatTheme.cpp
+++ src/UI/Styles/Themes/FlatTheme.cpp
@@ -11,7 +11,7 @@
 
 namespace UI::Themes
 {
-	static ColorCtx s_colors = {.primaryHue = 80, .secondaryHue = 150, .chroma = 0.0f, .darkMode = true};
+	static ColorCtx s_colors = {.primaryHue = 80, .secondaryHue = 30, .chroma = 0.0f, .darkMode = true};
 	static FontConfigSet s_fontConfigs = {
 		.header = {.size = 18, .style = LV_FREETYPE_FONT_STYLE_BOLD | LV_FREETYPE_FONT_STYLE_WEIGHT(700)},
 		.normal = {.size = 14, .style = LV_FREETYPE_FONT_STYLE_NORMAL | LV_FREETYPE_FONT_STYLE_WEIGHT(400)},
